909acd46ff3e1468c15d5e13b28601ae710f5990
Anshul Bhartiya <bhartiyaanshul@gmail.com>
2023-06-09 10:42:33 +0530

    commit

--- index.js
+++ index.js
@@ -1,47 +1,82 @@
+const { request } = require('express')
 let express = require('express')
 let app = express()
 app.use(express.json())
 let posts = []
-app.get('/home',(req,res)=>{
+app.get('/home', (req, res) => {
     res.send("Hello")
 })
-app.post('/createpost',(req,res)=>{
-    let obj=req.body
-    obj.id = posts.length+1
+app.post('/createpost', (req, res) => {
+    let obj = req.body
+    obj.id = posts.length + 1
     console.log(obj)
     posts.push(obj)
-    if(obj.title == "" && obj.desc== ""){
+    obj.isDelete = 0
+    if (obj.title == "" && obj.desc == "") {
         res.send({
-            isSuccess:false,
-           
-                
+            isSuccess: false,
         })
     }
-    else{
+    else {
         res.send({
-            isSuccess:true,
-            posts:posts
-                
-        })
-    }
-})
-app.get('/getpost',(req,res)=>{
-    let obj2 = req.query.id
-    let ans = posts.find((a)=>(
-        a.id==obj2
-        ))
+            isSuccess: true,
+            posts: posts,
+            
 
-      
-            res.send({
-                posts:ans
-                
-            })
-        
-        
+        })
+    }
+})
+
+app.get('/getpost', (req, res) => {
+    let obj2 = req.query.id
+    let ans = posts.find((a) => (
+        a.id == obj2
+    ))
+    res.send({
+        posts: ans
+
+    })
+})
+app.put('/updatepost',(req,res)=>{
+    let id = req.query.id
+    let idx = posts.findIndex((i)=>(
+        i.id == id
+    ))
+    posts[idx].title= req.body.title
+    posts[idx].desc = req.body.desc
+    res.send({
+        isSuccess: true,
+        posts:posts[idx],
+        message:"Updated Successfully"
+
+}   )
+})
+app.delete('/deletepost',(req,res)=>{
+    let id = req.query.id
+    let idx = posts.findIndex((i)=>(
+        i.id == id
+    ))
+    posts.splice(idx,1)
+    res.send({
+        isSuccess: true,
+        message: "Post Deleted"
+    })
+})
+app.delete('/softdeletepost',(req,res)=>{
+    let id = req.query.id
+    let idx = posts.findIndex((i)=>(
+        i.id == id
+    ))
+    posts[idx].isDelete = 1
     
+        res.send({
+            isSuccess: false,
+            posts: posts,
+        })
+   
     
 })
-app.listen(5001,()=>{
+app.listen(5001, () => {
     console.log("App is Started")
 })
 
